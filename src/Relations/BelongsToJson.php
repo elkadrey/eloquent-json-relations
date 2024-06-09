c20aa2029648a310f25d7c411da33bf8495ed861
Elkadrey <elkadrey@gmail.com>
2024-06-09 17:03:56 +0300

    fix

--- src/Relations/BelongsToJson.php
+++ src/Relations/BelongsToJson.php
@@ -187,7 +187,7 @@ class BelongsToJson extends BelongsTo
                 return Arr::get($value, $key) == $model->{$this->ownerKey};
             })->first();
 
-        return Arr::except($record, $key);
+        return Arr::except($record ?? [], $key);
     }
 
     /**
